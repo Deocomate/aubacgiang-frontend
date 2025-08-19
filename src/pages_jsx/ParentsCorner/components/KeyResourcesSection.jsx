8c4d81a893faf71d067e6d3f43bafe8ad834692f
no-copyright <datnd03.dev@gmail.com>
2025-08-19 12:06:02 +0700

    update

--- src/pages_jsx/ParentsCorner/components/KeyResourcesSection.jsx
+++ src/pages_jsx/ParentsCorner/components/KeyResourcesSection.jsx
@@ -1,14 +1,15 @@
-/* src/pages_jsx/ParentsCorner/components/KeyResourcesSection.jsx */
 import React from 'react';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import { BookOpen, Download, CalendarDays, MessageSquare, Milestone, Award  } from 'lucide-react';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Download, MessageSquare } from 'lucide-react';
 import Link from 'next/link';
+import Image from 'next/image';
+
 const resources = [
-    { title: 'Góc luyện tập online', description: 'Luyện đọc tiếng Anh online cùng con với Oxford Reading Club.', icon: <BookOpen className="h-8 w-8 text-orange-500" />, href: 'https://dashboard.oxfordreadingclub.com/' },
+    { title: 'Góc luyện tập online', description: 'Luyện đọc tiếng Anh online cùng con với Oxford Reading Club.', icon: <Image src="/assets/images/icon.jpeg" alt="Góc luyện tập online icon" width={32} height={32} className="h-8 w-8 rounded-full object-cover"/>, href: 'https://dashboard.oxfordreadingclub.com/' },
     { title: 'Tài liệu học tập', description: 'Tải về flashcards, bài tập giúp bé học tiếng Anh hiệu quả hơn', icon: <Download className="h-8 w-8 text-blue-500" />, href: '#downloads' },
-    // { title: 'Câu chuyện thành công', description: 'Những đánh giá tích cực của quý phụ huynh đã từng cho con theo học tại A&U', icon: <Award  className="h-8 w-8 text-green-500" />, href: '#testimonials' },
     { title: 'Làm bài tập tại nhà', description: 'Truy cập hệ thống bài tập về nhà online English Hub của Oxford.', icon: <MessageSquare className="h-8 w-8 text-purple-500" />, href: 'https://englishhub.oup.com/' },
 ];
+
 function KeyResourcesSection() {
     return (
         <section className="bg-gray-50 py-20 px-4 md:px-0">
